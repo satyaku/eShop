7303b866c74dcc8c8a2dddc4399630f15c25d41e
satyaku <satyam.bppimt@gmail.com>
2020-03-17 01:19:12 +0530

    Update EshopApplicationTests.java

--- src/test/java/com/shopping/eshop/EshopApplicationTests.java
+++ src/test/java/com/shopping/eshop/EshopApplicationTests.java
@@ -9,5 +9,19 @@ class EshopApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	/**
+	* Test case 1 - execute the method findAll() from the ProductsApplicationService and assert the response as not empty.
+	*/
+	
+	/**
+	* Test case 2 - execute the method getProductResponse() from the ProductsApplicationService eith all combinations of possible inputs 
+	* and assert the response as not empty.
+	*/
+	
+	/**
+	* Test case 3 - execute the method getCount() from the SellersApplicationService and assert if the count is being returned.
+	*/
+	
 
 }
